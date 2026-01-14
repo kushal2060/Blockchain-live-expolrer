7b3da71eba6f1ae58749c99688e01f69f59b20f5
Kushal Acharya <118195156+kushal2060@users.noreply.github.com>
2026-01-14 16:25:36 +0545

    changes in config

--- frontend/cardano_frontend/next.config.ts
+++ frontend/cardano_frontend/next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,        
-  swcMinify: true,            
-  output: "standalone",        
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
   },
-  typescript: {
-    ignoreBuildErrors: false,   
-  },
+
 };
 
 export default nextConfig;
